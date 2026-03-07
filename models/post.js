adaf38d759e913141f1dc002eac8acf71dac3f4d
Alehooom <ellafhassan7@gmail.com>
2026-03-07 13:34:43 +0100

    Lektion 1

---
+++ models/post.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes ) => {
+    return sequelize.define('post', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        }
+    }, 
+    {udnerscored: true}
+    );
+};
