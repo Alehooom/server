068e17af88b33e2ae5239969120c1f4bdeae0b67
Alehooom <ellafhassan7@gmail.com>
2026-03-10 09:55:54 +0100

    Lektion 2 - Del 2 - Modeller forts KLAR

--- models/tag.js
+++ models/tag.js
@@ -1,21 +1,16 @@
 module.exports = (sequelize, DataTypes ) => {
     return sequelize.define(
-        'comment', 
+        'tag', 
         {
         id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
         },
-        title: {
-            type:DataTypes.STRING(100),
-            allowNull: false,
-        },
-        body: {
-            type:DataTypes.TEXT,
-            allowNull: false,
-        },
-
+        name: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        }
     }, 
     {udnerscored: true}
     );
