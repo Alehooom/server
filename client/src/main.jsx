19f4bdfd1926236151ddcec5aff041033dff7543
Alehooom <ellafhassan7@gmail.com>
2026-03-13 15:25:03 +0100

    Lektion 4 - Del 4 - Routing och navigation KLAR

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,41 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import PostEdit from './views/PostEdit.jsx'
+import Posts from './views/Posts.jsx'
+import PostDetail from './views/PostDetail.jsx'
+import Home from './views/Home.jsx'
+
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  { path: '/', 
+    element:<App />, 
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/posts/new',
+        element: <PostEdit/>
+      },
+      {
+        path: '/posts/',
+        element: <Posts/>
+      },
+      {
+        path: '/posts/1',
+        element: <PostDetail/>
+      }
+    ]
+  }, 
+
+  {path: '/posts/new', element: <PostEdit/>}
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
